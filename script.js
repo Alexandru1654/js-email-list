e5b4828b187b0bd5cf3983b8b3396c271b041093
alexandru233847 <alexutualexino33@gmail.com>
2024-10-17 19:30:12 +0200

    Change fetch with axios

--- script.js
+++ script.js
@@ -2,29 +2,34 @@ const emailList = [];
 
 // Function for email
 function getEmails() {
+    const requests = [];
     for (let i = 0; i < 10; i++) {
-        fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(response => response.json())
-        .then(data => {
-            emailList.push(data.response);
-            if (emailList.length === 10) {
-                displayEmails();
-            }
-        })
-        .catch(error => console.error('Errore:', error));
+        requests.push(axios.get('https://flynn.boolean.careers/exercises/api/random/mail'));
     }
+
+    axios.all(requests)
+        .then(axios.spread((...responses) => {
+            responses.forEach(response => {
+                emailList.push(response.data.response);
+            });
+            displayEmails();
+        }))
+        .catch(error => console.error('Errore:', error));
 }
 
 // View emails in the dom
 function displayEmails() {
-    const ul = documentgetElementById('email-list');
+    const ul = document.getElementById('email-list');
     emailList.forEach(email => {
         const li = document.createElement('li');
         li.textContent = email;
         ul.appendChild(li);
     });
-};
+}
 
 // Email recovery process
 getEmails();
+
+
+
 
